31aac30fa2cd88752eb0b5282e8e9ca37ec659ec
LenaBystrykh <lena.bystryh@mail.ru>
2024-04-23 11:58:46 +0500

    fix appflow settings

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,13 +5,30 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    <p @click="checkLocal">проверить</p>
+    <p>{{ result }}</p>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const result = ref('')
+
+async function checkLocal () {
+  const options = {
+    method: 'GET',
+    url: 'http://line1a.local',
+    url: 'https://line1a.local',
+    headers: { Accept: 'application/json' },
+  }
+  try {
+    const { data } = await axios.request(options)
+    console.log(data)
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
 </script>
