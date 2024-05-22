85f4c33a5d6ece93c00c6059017838416234a3d6
LenaBystrykh <lena.bystryh@mail.ru>
2024-05-22 12:36:53 +0500

    test: try to use protobuf

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,41 @@
 <template>
-  <p @click="checkLocal">Проврить</p>
-  <p>{{ result }}</p>
+  <div class="start-scan-page">
+    <p @click="checkLocal">Чтобы начать работу, отсканируйте личный код пользователя</p>
+    <p>{{ result }}</p>
+    <div class="start-scanner-wrapper">
+      <BaseScanner :height="'calc(100% - 32px)'" :width="'auto'" :code-type="'DataMatrix'"></BaseScanner>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import { createPromiseClient } from '@connectrpc/connect'
 import { createConnectTransport } from '@connectrpc/connect-web'
-import { Job } from '../gen/job_connect'
+import { Token } from '../gen/token_connect'
+
+import protobuf from 'protobufjs'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import BaseScanner from '../components/BaseScanner.vue'
+import {
+  BarcodeScanner,
+  BarcodeFormat
+  // LensFacing,
+} from '@capacitor-mlkit/barcode-scanning'
+const isMobile = ref(false)
+const scanActive = ref(false)
 const result = ref('')
+const scanOptions = {
+  formats: [BarcodeFormat.DataMatrix]
+}
+const isSupported = async () => {
+  try {
+    const { supported } = await BarcodeScanner.isSupported()
+    return supported
+  } catch (e) {
+    console.log(e)
+  }
+  return false
+}
 
 async function sha256 (message) {
   const msgBuffer = new TextEncoder().encode(message)
@@ -23,15 +50,65 @@ async function checkLocal () {
   const transport = createConnectTransport({
     baseUrl: import.meta.env.VITE_URL
   })
-  const client = createPromiseClient(Job, transport)
+  const client = createPromiseClient(Token, transport)
   const signParams = import.meta.env.VITE_UUID + ':' + import.meta.env.VITE_SECRET
   const signEncrypted = await sha256(signParams)
   const response = await client.list({
     token: import.meta.env.VITE_UUID,
     sign: signEncrypted
   })
+  console.log('response:')
   console.log(response)
 }
+
+const startScan = async () => {
+  document.querySelector('body')?.classList.add('barcode-scanner-active')
+  scanActive.value = true
+  await BarcodeScanner.addListener(
+    'barcodeScanned',
+    async result => {
+      console.log('startscan result:')
+      console.log(result)
+      await updateResult(result.barcode.displayValue)
+      stopScan()
+    }
+  )
+  await BarcodeScanner.startScan(scanOptions)
+}
+async function updateResult (val) {
+  const root = await protobuf.load('src/assets/product.proto')
+  const Product = root.lookupType('Auth')
+  const product = Product.create(val)
+
+  const binData = Product.encode(product).finish()
+  const asciiData = String.fromCharCode.apply(null, binData)
+
+  const decodeBinData = Uint8Array.from(asciiData, c => c.charCodeAt(0))
+  const decodeProduct = Product.decode(decodeBinData)
+  console.log(`Decode product: ${JSON.stringify(decodeProduct)}`)
+}
+
+const stopScan = async () => {
+  document.querySelector('body')?.classList.remove('barcode-scanner-active')
+  scanActive.value = false
+  await BarcodeScanner.removeAllListeners()
+  await BarcodeScanner.stopScan()
+}
+
+onMounted(async () => {
+  isMobile.value = await isSupported()
+  if (isMobile.value) {
+    await startScan()
+  } else {
+    await updateResult({token:'123', secret:'1245'})
+  }
+})
+
+onBeforeUnmount(async () => {
+  if (isMobile.value) {
+    await stopScan()
+  }
+})
 </script>
 
 <style lang="scss">
